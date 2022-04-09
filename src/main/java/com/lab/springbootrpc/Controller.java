2fcb3153a712ecbd719119672383a2a88a938415
huynv24 <huynv24@cyberspace.vn>
2022-04-09 21:03:03 +0700

    add

--- src/main/java/com/lab/springbootrpc/Controller.java
+++ src/main/java/com/lab/springbootrpc/Controller.java
@@ -1,5 +1,6 @@
 package com.lab.springbootrpc;
 
+import com.lab.springbootrpc.Service.single_queue.MQSingleQueuePublisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,17 @@ public class Controller {
 
     @Autowired
     private com.lab.springbootrpc.Service.MQPublisher MQPublisher;
+    @Autowired
+    private MQSingleQueuePublisher mqSingleQueuePublisher;
+
 
     @GetMapping("/")
     public void demo(){
         MQPublisher.send();
+    }
+
+    @GetMapping("/single")
+    public void single(){
+        mqSingleQueuePublisher.send();
     }
 }
